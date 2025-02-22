de819df7a15a0f6feaf2dc3acafecb1ebfc12559
AMRITPAL SINGH THETHI <amritpalsinghthethi@gmail.com>
2025-02-23 01:49:55 +0530

    added endpoint to get specific transactions

--- src/main/java/com/asthethi/docprocessor/controller/BankStatementPdfController.java
+++ src/main/java/com/asthethi/docprocessor/controller/BankStatementPdfController.java
@@ -1,34 +1,34 @@
 package com.asthethi.docprocessor.controller;
 
 import com.asthethi.docprocessor.model.FileRequest;
+import com.asthethi.docprocessor.model.Transaction;
 import com.asthethi.docprocessor.service.PdfProcessorService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.List;
 
 @RestController
 @AllArgsConstructor
-@RequestMapping("pdf/statement")
+@RequestMapping("account/statement")
 public class BankStatementPdfController {
 
     private PdfProcessorService pdfProcessorService;
 
-    @GetMapping("/gettext")
-    public ResponseEntity<List<Transaction>> getAllTransactionsFromFile(@ModelAttribute FileRequest fileRequest) throws IOException {
+    @GetMapping("/transactions")
+    public ResponseEntity<List<Transaction>> getAllPdfText(@ModelAttribute FileRequest fileRequest) throws IOException {
         return ResponseEntity.status(HttpStatus.OK).
                 body(pdfProcessorService.getAllPdfText(fileRequest));
     }
 
-    @GetMapping("/test")
-    public ResponseEntity<String> testEndpoint(){
-        return ResponseEntity.status(HttpStatus.OK).
-                body("Test Success");
+    @GetMapping("/transactions/{transactionCategory}")
+    public ResponseEntity<List<Transaction>> getSpecificCategoryTransactions(@PathVariable("transactionCategory") String transactionCategory,
+                                                                             @RequestParam MultipartFile document) throws IOException {
+        List<Transaction> transactions = pdfProcessorService.getSpecificCategoryTransactions(transactionCategory, document);
+        return ResponseEntity.status(HttpStatus.OK).body(transactions);
     }
 }
