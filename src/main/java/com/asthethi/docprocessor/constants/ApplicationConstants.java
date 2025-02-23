b4db27805b57abd6413f16b62332d255449c6692
AMRITPAL SINGH THETHI <amritpalsinghthethi@gmail.com>
2025-02-23 12:49:00 +0530

    added constants

--- src/main/java/com/asthethi/docprocessor/constants/ApplicationConstants.java
+++ src/main/java/com/asthethi/docprocessor/constants/ApplicationConstants.java
@@ -3,4 +3,7 @@ package com.asthethi.docprocessor.constants;
 public class ApplicationConstants {
     public static final String TEXT_FILE_NOT_SUPPORTED_ERR = "Currently Text file type is not supported";
     public static final String UNSUPPORTED_DOCUMENT_TYPE_ERR  = "Provided Document Type Not Support";
+    public static final String HDFC_BANK_STRING = "HDFC BANK LTD";
+    public static final String TRANSACTION_CATEGORY_ACH = "ACH";
 }
+
