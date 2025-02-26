06eede027b3843be585d553a0e5787a978f47c91
Sunpreet Singh <asthethi>
2025-02-26 22:57:03 +0530

    added open api dependency

---
+++ src/main/java/com/asthethi/docprocessor/swagger/config/OpenApiConfig.java
@@ -0,0 +1,10 @@
+//package com.asthethi.docprocessor.swagger.config;
+//
+//import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+//import io.swagger.v3.oas.annotations.servers.Server;
+//
+//
+//@OpenAPIDefinition(servers = {@Server(description = "Local Server",
+//        url = "http://localhost:8088/")})
+//public class OpenApiConfig {
+//}
